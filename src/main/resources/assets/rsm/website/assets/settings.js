68da74cedc2592e47dec62c634e2cbf962e119a7
LX862 <kenny.mh.hui@outlook.com>
2023-03-19 16:26:47 +0800

    Fix URL

--- src/main/resources/assets/rsm/website/assets/settings.js
+++ src/main/resources/assets/rsm/website/assets/settings.js
@@ -1,6 +1,6 @@
 export const SETTINGS = {
-    // BASE_URL: document.location.origin + document.location.pathname.replace("index.html", ""),
-    BASE_URL: "http://localhost:10290/",
+    BASE_URL: document.location.origin + document.location.pathname.replace("index.html", ""),
+    // BASE_URL: "http://localhost:10290/",
     // BASE_URL: "https://www.joban.tk/rsminternal/",
     // BASE_URL: "https://rsm.joban.tk/",
     LINE_THICKNESS: 4,
